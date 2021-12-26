21066aa4bbe363d253ed48f111bd485d657c5b36
Harly <academyynetworking@gmail.com>
2021-12-26 20:19:21 +0800

    Update: add property query users

--- tests/UsersTableTestHelper.js
+++ tests/UsersTableTestHelper.js
@@ -2,11 +2,9 @@
 const pool = require('../src/Infrastructures/database/postgres/pool');
 
 const UsersTableTestHelper = {
-  async addUser({
-    id = 'user-123', username = 'dicoding', password = 'secret', fullname = 'Dicoding Indonesia',
-  }) {
+  async addUser({ id = 'user-123', username = 'dicoding', password = 'secret', fullname = 'Dicoding Indonesia' }) {
     const query = {
-      text: 'INSERT INTO users VALUES($1, $2, $3, $4)',
+      text: 'INSERT INTO users (id, username, password, fullname) VALUES($1, $2, $3, $4)',
       values: [id, username, password, fullname],
     };
 
