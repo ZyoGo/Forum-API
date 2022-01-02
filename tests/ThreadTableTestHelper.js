896396c256f9103c49bd9a650557a4e38fb08a53
Harly <academyynetworking@gmail.com>
2022-01-02 09:09:13 +0800

    Update: add date valye on addThread

--- tests/ThreadTableTestHelper.js
+++ tests/ThreadTableTestHelper.js
@@ -5,11 +5,12 @@ const ThreadTableTestHelper = {
     id = 'Dicoding-123',
     title = 'Dicoding Thread',
     body = 'Thread from Dicoding Indonesia',
+    date = '2021-12-26T12:48:06.733Z',
     owner = 'user-123',
   }) {
     const query = {
-      text: 'INSERT INTO threads (id, title, body, owner_id) VALUES ($1, $2, $3, $4) RETURNING id, title, owner_id as owner',
-      values: [id, title, body, owner],
+      text: 'INSERT INTO threads (id, title, body, date, owner_id) VALUES ($1, $2, $3, $4, $5) RETURNING id, title, owner_id as owner',
+      values: [id, title, body, date, owner],
     };
 
     const result = await pool.query(query);
